44e7b6c989ded98a847690ddaeb63987548be77d
Koki Tanaka <mail@gojiteji.com>
2023-01-19 22:26:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ acct = Account.from_key(private_key)
       
 # get signature from wallet
 signed=acct.signHash(challenge_txt)
-signed=signed["messageHash"]
+signed=string(signed["messageHash"])
 print("-----------")
 print(signed)
 print("-----------")
